6098e2a58e7eed926e80d85e53aa8dd83c83513f
t.Hunt <39423683+Plumtree74@users.noreply.github.com>
2018-08-12 16:12:11 -0600

    Update control_register.sv
    provides a rough implementation of the control_register.

--- src/hdl/control_register.sv
+++ src/hdl/control_register.sv
@@ -11,13 +11,42 @@
 /// *   [write_register] is the value to be written to the control regiater when
 ///     the [write] signal rises.
 /// *   [control] is the current value of the control register.
+
 module control_register(
     input logic clk,
     input logic reset,
     input logic write,
     input logic [7:0] write_register,
-    output logic control[7:0]);
-
-    // TODO
+    //                 TEMPORARY NAMES    
+    output logic [1:0] setting0, 
+                       setting1, 
+                       setting2, 
+                       setting3, 
+                       setting4, 
+                       setting5, 
+                       setting6,
+                       setting7 );
+              
+    always_ff @(posedge clk) begin
+        if (reset) begin
+            setting0 <= 0;
+            setting1 <= 0;
+            setting2 <= 0;
+            setting3 <= 0;
+            setting4 <= 0;
+            setting5 <= 0;
+            setting6 <= 0;
+            setting7 <= 0;
+        end else if (write) begin
+            setting0 <= write_register[0];
+            setting1 <= write_register[1];
+            setting2 <= write_register[2];
+            setting3 <= write_register[3];
+            setting4 <= write_register[4];
+            setting5 <= write_register[5];
+            setting6 <= write_register[6];
+            setting7 <= write_register[7];
+        end
+    end
 
 endmodule
