2392849a0bb978e722c004d54763b25663b8d257
t.Hunt <39423683+Plumtree74@users.noreply.github.com>
2018-08-29 17:21:35 -0600

    Update main.sv
    Adds a possibly temporary implementation to the top level main module in "main.sv" in order to link the project together finally.

--- src/hdl/main.sv
+++ src/hdl/main.sv
@@ -8,7 +8,6 @@
 /// *   [reset] is the synchronous active high reset signal.
 /// *   [rx] is the receiving serial line.
 /// *   [tx] is the transmitting serial line.
-/// *   [digital] is a bus of digital inputs for the logic analyzer.
 /// *   [analog_p] is a bus of positive sides of the anlog inputs.
 /// *   [analog_n] is a bus of negative sides of the anlog inputs.
 module main(
@@ -16,14 +15,51 @@ module main(
     input logic reset,
     input logic rx,
     output logic tx,
-    input logic [7:0] digital,
     input logic [1:0] analog_p,
     input logic [1:0] analog_n);
-
-    // TODO Derive clocks using `clk_ref`. `clk_ref` should not be forwarded to
-    //      any other modules because a clock coming from a clock management
-    //      tile will have higher stability.
-
-    // TODO Connect the rest of everything up
-
+    
+    logic [23:0] data;
+    logic [7:0] settings;
+    logic [3:0][7:0] send_data;
+    logic write_ready;
+    logic send_ready;
+    
+    assign send_data[0][7:0] = settings;
+    assign send_data[1][7:0] = data[7:0];
+    assign send_data[2][7:0] = data[15:8];
+    assign send_data[3][7:0] = data[23:16];
+    
+    usb_communicator usb_communicator (
+        .clk(clk_ref),
+        .reset(reset),
+        .data(send_data),
+        .rx(rx),
+        .tx(tx),
+        .control(settings)
+    );
+    
+    control_register control_register(
+        .clk(clk_ref),
+        .reset(reset),
+        .write(write_ready),
+        .write_register(settings),
+        .setting0(settings[0]),
+        .setting1(settings[1]),
+        .setting2(settings[2]),
+        .setting3(settings[3]),
+        .setting4(settings[4]),
+        .setting5(settings[5]),
+        .setting6(settings[6]),
+        .setting7(settings[7])
+    );
+    
+    signal_reader signal_reader(
+        .clk(clk_ref),
+        .reset(reset),
+        .analog_p(analog_p),
+        .analog_n(analog_n),
+        .ready(write_ready),
+        .analog_out(data)
+    );
+    
 endmodule
