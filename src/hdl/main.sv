87161209673be51a9b82b0672e7fc9e76f1898a6
t.Hunt <39423683+Plumtree74@users.noreply.github.com>
2018-09-06 18:08:52 -0600

    Update main.sv
    changes to reflect the decision to only have one setting, on/off.

--- src/hdl/main.sv
+++ src/hdl/main.sv
@@ -19,20 +19,15 @@ module main(
     input logic [1:0] analog_n);
     
     logic [23:0] data;
-    logic [7:0] settings;
-    logic [3:0][7:0] send_data;
+    logic settings;
+    logic toggled_on;
     logic write_ready;
-    logic send_ready;
-    
-    assign send_data[0][7:0] = settings;
-    assign send_data[1][7:0] = data[7:0];
-    assign send_data[2][7:0] = data[15:8];
-    assign send_data[3][7:0] = data[23:16];
     
     usb_communicator usb_communicator (
         .clk(clk_ref),
         .reset(reset),
-        .data(send_data),
+        .toggled_on(toggled_on),
+        .data(data),
         .rx(rx),
         .tx(tx),
         .control(settings)
@@ -43,14 +38,7 @@ module main(
         .reset(reset),
         .write(write_ready),
         .write_register(settings),
-        .setting0(settings[0]),
-        .setting1(settings[1]),
-        .setting2(settings[2]),
-        .setting3(settings[3]),
-        .setting4(settings[4]),
-        .setting5(settings[5]),
-        .setting6(settings[6]),
-        .setting7(settings[7])
+        .toggled_on(toggled_on)
     );
     
     signal_reader signal_reader(
